0757bc3e5a40f74bcc4705193177789052463950
Dan Fornika <dfornika@gmail.com>
2016-10-26 15:59:52 -0700

    Added solution to exercise 5

---
+++ test/clojure_exercises/exercise_005.clj
@@ -0,0 +1,16 @@
+(ns clojure-exercises.exercise-005
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 5
+;;
+;; <p>When operating on a list, the conj function will return a new list with one or more items "added" to the front.</p>
+;;
+;; <p>Note that there are two test cases, but you are expected to supply only one answer, which will cause all the tests to pass.</p>
+
+(def __
+  '(1 2 3 4)
+  )
+
+(deftest tests
+  (is (= __ (conj '(2 3 4) 1)))
+  (is (= __ (conj '(3 4) 2 1))))
