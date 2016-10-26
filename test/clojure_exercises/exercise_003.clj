904b151805b04e59ec2ab719684801664ea72a18
Dan Fornika <dfornika@gmail.com>
2016-10-26 13:29:53 -0700

    Added solution for exercise 3

---
+++ test/clojure_exercises/exercise_003.clj
@@ -0,0 +1,13 @@
+(ns clojure-exercises.exercise-003
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 3
+;;
+;; Clojure strings are Java strings.  This means that you can use any of the Java string methods on Clojure strings.
+
+(def __
+  "HELLO WORLD"
+  )
+
+(deftest tests
+  (is (= __ (.toUpperCase "hello world"))))
