54a246bd87a7fd8a5ac5cf2cee32873afefd9f1c
Dan Fornika <dfornika@gmail.com>
2016-10-26 13:31:42 -0700

    Removed lein-sub plugin

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-sub "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
