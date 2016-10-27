f5463d6132a0560298b902658e7165e1e20f03cc
Dan Fornika <dfornika@gmail.com>
2016-10-27 11:16:07 -0700

    Added solution to exercise 21

---
+++ test/clojure_exercises/exercise_021.clj
@@ -0,0 +1,18 @@
+(ns clojure-exercises.exercise-021
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 21
+;;
+;; Write a function which returns the Nth element from a sequence.
+;;
+;; Restrictions (please don't use these function(s)): nth
+
+(def __
+  #(first (drop %2 %1))
+  )
+
+(deftest tests
+  (is (= (__ '(4 5 6 7) 2) 6))
+  (is (= (__ [:a :b :c] 0) :a))
+  (is (= (__ [1 2 3 4] 1) 2))
+  (is (= (__ '([1 2] [3 4] [5 6]) 2) [5 6])))
