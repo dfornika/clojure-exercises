e065681a4a40a19ba9b0c19009973e7e16e100f1
Dan Fornika <dfornika@gmail.com>
2016-10-26 21:40:51 -0700

    Added solution to exercise 25

---
+++ test/clojure_exercises/exercise_025.clj
@@ -0,0 +1,16 @@
+(ns clojure-exercises.exercise-025
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 25
+;;
+;; Write a function which returns only the odd numbers from a sequence.
+
+(def __
+  (fn [seq] (filter #(= (rem % 2) 1) seq))
+  )
+
+(deftest tests
+  (is (= (__ #{1 2 3 4 5}) '(1 3 5)))
+  (is (= (__ [4 2 1 6]) '(1)))
+  (is (= (__ [2 2 4 6]) '()))
+  (is (= (__ [1 1 1 3]) '(1 1 1 3))))
