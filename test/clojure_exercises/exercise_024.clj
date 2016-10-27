4434454cdef5c4f2f37e7d40a49d72bd49f182a0
Dan Fornika <dfornika@gmail.com>
2016-10-26 21:40:43 -0700

    Added solution to exercise 24

---
+++ test/clojure_exercises/exercise_024.clj
@@ -0,0 +1,17 @@
+(ns clojure-exercises.exercise-024
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 24
+;;
+;; Write a function which returns the sum of a sequence of numbers.
+
+(def __
+  #(reduce + 0 %)
+  )
+
+(deftest tests
+  (is (= (__ [1 2 3]) 6))
+  (is (= (__ (list 0 -2 5 5)) 8))
+  (is (= (__ #{4 2 1}) 7))
+  (is (= (__ '(0 0 -1)) -1))
+  (is (= (__ '(1 10 3)) 14)))
