45aca7a06a9693adc55b2e96addb835ea826bdbc
Dan Fornika <dfornika@gmail.com>
2016-10-26 17:06:55 -0700

    Added solution for exercise 6

---
+++ test/clojure_exercises/exercise_006.clj
@@ -0,0 +1,16 @@
+(ns clojure-exercises.exercise-006
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 6
+;;
+;; Vectors can be constructed several ways.  You can compare them with lists.
+;;
+;; <br/><br/><b>Note</b>: the brackets [] surrounding the blanks __ are part of the test case.
+
+;; Note: dfornika 2016-10-26: Had to cheat a bit, can't def __ to three bare keywords
+(def __
+  [:a :b :c]
+  )
+
+(deftest tests
+  (is (= __ (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))))
