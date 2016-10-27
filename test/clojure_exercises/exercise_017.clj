a930cf6297a8e9b40f9a84851cd0460e63a524ee
Dan Fornika <dfornika@gmail.com>
2016-10-27 10:40:26 -0700

    Added solution for exercise 17

---
+++ test/clojure_exercises/exercise_017.clj
@@ -0,0 +1,13 @@
+(ns clojure-exercises.template
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 17
+;;
+;; The map function takes two arguments: a function (f) and a sequence (s).  Map returns a new sequence consisting of the result of applying f to each item of s.  Do not confuse the map function with the map data structure.
+
+(def __
+  '(6 7 8)
+  )
+
+(deftest tests
+  (is (= __ (map #(+ % 5) '(1 2 3)))))
