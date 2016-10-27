3d6d4e4a675a2e2e8d42780603757dbb6edbbe4d
Dan Fornika <dfornika@gmail.com>
2016-10-26 21:47:54 -0700

    Added solution for exercise 13

---
+++ test/clojure_exercises/exercise_013.clj
@@ -0,0 +1,13 @@
+(ns clojure-exercises.exercise-013
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 13
+;;
+;; The rest function will return all the items of a sequence except the first.
+
+(def __
+  '(20 30 40)
+  )
+
+(deftest tests
+  (is (= __ (rest [10 20 30 40]))))
