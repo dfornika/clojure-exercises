01a5a2939e9e8f3d75d7614d146ad82c43289791
Dan Fornika <dfornika@gmail.com>
2016-10-27 10:59:49 -0700

    Added solution to exercise 20

---
+++ test/clojure_exercises/exercise_020.clj
@@ -0,0 +1,15 @@
+(ns clojure-exercises.exercise-020
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 20
+;;
+;; Write a function which returns the second to last element from a sequence.
+
+(def __
+  (fn [x] (first (rest (reverse x))))
+  )
+
+(deftest tests
+  (is (= (__ (list 1 2 3 4 5)) 4))
+  (is (= (__ ["a" "b" "c"]) "b"))
+  (is (= (__ [[1 2] [3 4]]) [1 2])))
