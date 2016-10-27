362b4e813a77aaab5ed2bf4a0ba1ca5b84b1ec54
Dan Fornika <dfornika@gmail.com>
2016-10-26 22:07:03 -0700

    Added solution to exercise 15

---
+++ test/clojure_exercises/exercise_015.clj
@@ -0,0 +1,16 @@
+(ns clojure-exercises.exercise-015
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 15
+;;
+;; Write a function which doubles a number.
+
+(def __
+  #(* 2 %)
+  )
+
+(deftest tests
+  (is (= (__ 2) 4))
+  (is (= (__ 3) 6))
+  (is (= (__ 11) 22))
+  (is (= (__ 7) 14)))
