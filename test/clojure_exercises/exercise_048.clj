05b168ec18376cdc7bca26780ee3e84970906d6b
Dan Fornika <dan@fornikascience.com>
2016-10-31 22:15:53 -0700

    Added solution to exercise 48

---
+++ test/clojure_exercises/exercise_048.clj
@@ -0,0 +1,14 @@
+(ns clojure-exercises.exercise-048
+  (:require [clojure.test :refer :all]))
+
+;; 4Clojure Question 48
+;;
+;; The some function takes a predicate function and a collection.  It returns the first logical true value of (predicate x) where x is an item in the collection.
+
+(def __
+  6
+  )
+
+(deftest tests
+  (is (= __ (some #{2 7 6} [5 6 7 8])))
+  (is (= __ (some #(when (even? %) %) [5 6 7 8]))))
